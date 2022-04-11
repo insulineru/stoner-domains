369784e09f3589d567779f377664db90a757effd
Aleksey Lisun <insuline.ru@gmail.com>
2022-04-11 19:19:01 +0300

    ✨ feat: add mint button logic (connect contract)

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
-import React, { FC, useEffect, useState } from 'react'
+import { ethers } from 'ethers'
+import React, { FC, FormEvent, useEffect, useState } from 'react'
 import ApeImage from './assets/images/ape-transparent.png'
+import { Domains__factory } from './types/factories/Domains__factory'
 
 const DOMAIN_TLD = '.stoner'
+const CONTRACT_ADDRESS = '0x0ebd3fda2e512606a1f4fb2cff8ab888fae3c85b'
 
 export const App: FC = () => {
   const [currentAccount, setCurrentAccount] = useState('')
@@ -39,10 +42,45 @@ export const App: FC = () => {
     }
   }
 
-  const onMint = async (e: SubmitEvent) => {
+  const onMint = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault()
-    console.log('Minting', domain, record)
-    return false
+    if (!domain || !record) return
+
+    const price =
+      domain.length === 3 ? '0.5' : domain.length === 4 ? '0.3' : '0.1'
+    console.log('Minting domain', domain, 'with price', price)
+
+    try {
+      const { ethereum } = window
+      if (!ethereum) return
+
+      // @ts-ignore
+      const provider = new ethers.providers.Web3Provider(ethereum)
+      const signer = provider.getSigner()
+      const contract = Domains__factory.connect(CONTRACT_ADDRESS, signer)
+
+      let tx = await contract.register(domain, {
+        value: ethers.utils.parseEther(price),
+      })
+      const receipt = await tx.wait()
+
+      if (receipt.status === 1) {
+        console.log(
+          `Domain minted! https://mumbai.polygonscan.com/tx/${tx.hash}`,
+        )
+
+        tx = await contract.setRecord(domain, record)
+        await tx.wait()
+        console.log(`Record set! https://mumbai.polygonscan.com/tx/${tx.hash}`)
+
+        setRecord('')
+        setDomain('')
+      } else {
+        alert('Transaction is failed. Please, try again') // todo: Use toasts
+      }
+    } catch (e) {
+      console.log(e)
+    }
   }
 
   const renderNotConnectedContainer = () => (
@@ -61,11 +99,12 @@ export const App: FC = () => {
   )
 
   const renderForm = () => (
-    <form onSubmit={onMint}>
+    <form onSubmit={(e) => onMint(e)}>
       <h1 className="text-7xl font-black text-white max-w-lg">
         Okey, let's build your domain!
       </h1>
       <div className="mt-8 flex rounded-md shadow-sm">
+        {/* TODO: Add validate rules (min 3 length) */}
         <input
           type="text"
           className="flex-1 min-w-0 block rounded-l-md w-full px-3 py-2 bg-gray-700 text-white font-bold sm:text-sm"
